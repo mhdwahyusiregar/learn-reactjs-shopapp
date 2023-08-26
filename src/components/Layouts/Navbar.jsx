a9e336d17129fbd7b9d6ab54541ccb9be6444f01
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-08-26 17:05:24 +0700

    implementasi usecontext

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useLogin } from '../../hooks/useLogin';
 import Button from '../Elements/Button';
 import { useSelector } from 'react-redux';
+import { DarkMode } from '../../context/DarkMode';
 const Navbar = () => {
   const username = useLogin();
   const [totalCart, setTotalCart] = useState(0);
   const cart = useSelector((state) => state.cart.data);
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
 
   useEffect(() => {
     const sum = cart.reduce((acc, item) => {
@@ -23,9 +25,15 @@ const Navbar = () => {
       <Button classname="ml-5 bg-black" onClick={handleLogout}>
         Logout
       </Button>
-      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5">
+      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5 mr-5">
         {totalCart}
       </div>
+      <Button
+        className="bg-dark px-10 mx-5 text-white rounded"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? 'Light' : 'Dark'}
+      </Button>
     </div>
   );
 };
