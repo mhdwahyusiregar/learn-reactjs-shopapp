bb0bd242c05e69e51c659c9523a5e9aec82eb751
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-08-26 15:39:13 +0700

    implementasi react-redux

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -3,11 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    data: [{ id: 1, qty: 1 }],
+    data: JSON.parse(localStorage.getItem('cart')) || [],
   },
   reducers: {
     addToCart: (state, action) => {
-      state.data.push(action.payload);
+      const itemInCart = state.data.find(
+        (item) => item.id === action.payload.id,
+      );
+      if (itemInCart) {
+        itemInCart.qty++;
+      } else {
+        state.data.push(action.payload);
+      }
     },
   },
 });
